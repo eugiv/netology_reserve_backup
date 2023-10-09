575e64e373902d837bc4239cee3d05a1b8e02cf3
eugiv <eugene.ivanov@ymail.com>
2023-10-09 17:29:14 +0300

    added loading photos to Yandex Dissc folder

--- reserve_backup.py
+++ reserve_backup.py
@@ -1,11 +1,14 @@
 import requests
 import json
 from pprint import pprint
+from datetime import datetime
+from tqdm import tqdm
+import time
 
 
 class VK:
-    def __init__(self, access_token, user_id, version='5.154'):
-        self.token = access_token
+    def __init__(self, access_token_vk, user_id, version='5.154'):
+        self.token = access_token_vk
         self.id = user_id
         self.version = version
         self.base_params = {'access_token': self.token, 'v': self.version}
@@ -17,29 +20,36 @@ class VK:
         response = requests.get(url, params={**self.base_params, **params})
         return response.json()
 
-    def photos_info(self):
+    def photos_raw_data(self):
         url = f'{self.api_base_url}/method/photos.get'
-        params = {'owner_id': self.id, "album_id": "wall", "extended": 1}
+        params = {'owner_id': self.id,
+                  "album_id": "wall",
+                  "extended": True}
         response = requests.get(url, params={**self.base_params, **params})
         photos = response.json()
 
         photos_raw_data = []
-        for key in photos["response"]["items"]:
+        for key in tqdm(photos["response"]["items"], desc="Creating raw photos metadata"):
+            time.sleep(0.05)
             max_photo_size = max(key["sizes"], key=lambda x: x.get("height", 0) + x.get("width", 0))
             raw_data = {"likes": key["likes"]["count"],
-                        "upload_date": key["date"],
+                        "upload_date": datetime.utcfromtimestamp(key["date"]).strftime('%Y-%m-%d'),
                         "photo_link": max_photo_size["url"],
                         "size": max_photo_size["type"],
                         "photo_id": key["id"]}
 
             photos_raw_data.append(raw_data)
+        return photos_raw_data
 
-        likes_list = [x["likes"] for x in photos_raw_data]
-        dates_list = [x["upload_date"] for x in photos_raw_data]
+    def photos_file(self):
+        photos_data = self.photos_raw_data()
+        likes_list = [x["likes"] for x in photos_data]
+        dates_list = [x["upload_date"] for x in photos_data]
         mutual_list_cnt = list(zip(likes_list, dates_list))
 
         files_info = []
-        for each_dict, mut in zip(photos_raw_data, mutual_list_cnt):
+        for each_dict, mut in tqdm(zip(photos_data, mutual_list_cnt), desc="Making metadata json"):
+            time.sleep(0.05)
             likes_counter = likes_list.count(each_dict["likes"])
             dates_counter = dates_list.count(each_dict["upload_date"])
             mutual_counter = mutual_list_cnt.count(mut)
@@ -53,14 +63,23 @@ class VK:
 
             files_info.append({"file_name": each_dict["file_name"],
                                "size": each_dict["size"]})
-        pprint(files_info)
+
+        with open("vk_photofiles_metadata.json", "w") as f:
+            json.dump(files_info, f, indent=2)
+
+        return files_info
+
+    def photos_links(self):
+        photos_url = self.photos_raw_data()
+        photos_url = [x.get("photo_link", "") for x in photos_url]
+        return photos_url
 
 
 class YA:
-    def __init__(self, access_token):
-        self.token = access_token
+    def __init__(self, access_token_ya):
+        self.token = access_token_ya
         self.api_base_url = "https://cloud-api.yandex.net"
-        self.base_headers = {"Authorization": access_token}
+        self.base_headers = {"Authorization": access_token_ya}
 
     def ya_create_folder(self):
         url = f'{self.api_base_url}/v1/disk/resources'
@@ -71,25 +90,36 @@ class YA:
 
     def ya_qet_load_link(self):
         url = f'{self.api_base_url}/v1/disk/resources/upload'
-        params = {"path": f"{self.ya_create_folder()}/requirements.txt"}
-        url_for_load = requests.get(url, headers=self.base_headers, params=params).json()
-        return url_for_load.get("href", "")
+
+        links_ya_upload = []
+        for photo_name in tqdm(vk.photos_file(), desc="Performing links for upload to Yandex Disc"):
+            time.sleep(0.05)
+            photo_name = photo_name.get("file_name", "")
+            params = {"path": f"{self.ya_create_folder()}/{photo_name}",
+                      "overwrite": True}
+            url_for_load = requests.get(url, headers=self.base_headers, params=params).json()
+            links_ya_upload.append(url_for_load.get("href", ""))
+
+        return links_ya_upload
 
     def ya_load_photos(self):
-        with open("requirements.txt", "rb") as f:
-            requests.put(self.ya_qet_load_link(), files={"file": f})
+        for link_ya, link_vk in tqdm(zip(self.ya_qet_load_link(), vk.photos_links()),
+                                     desc="Uploading photos to a Yandex Disc folder"):
+            time.sleep(0.05)
+            vk_files = requests.get(link_vk).content
+            requests.put(link_ya, files={"file": vk_files})
 
 
 with open('tokens.txt') as f:
     file = json.loads(f.read())
-    access_token_vk = file["token_vk"]
+    access_token_vkt = file["token_vk"]
     user_id = file["access_id"]
-    access_token_ya = file["token_ya"]
+    access_token_yan = file["token_ya"]
 
-vk = VK(access_token_vk, user_id)
-# vk.photos_info()
-# vk.users_info()
+vk = VK(access_token_vkt, user_id)
+# vk.photos_links()
+# vk.photos_raw_data()
+# vk.photos_file()
 
-ya = YA(access_token_ya)
+ya = YA(access_token_yan)
 ya.ya_load_photos()
-
